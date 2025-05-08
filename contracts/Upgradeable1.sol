c041f53ed82acca9dfa799b4c6ce1c8c601139bd
Victor Chen <victor.chen@hoc-trade.com>
2025-05-08 16:27:11 +0800

    feature: implement one upgradeable contract

---
+++ contracts/Upgradeable1.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract Upgradeable1 is Initializable, ERC20Upgradeable {
+    function initialize(string memory name, string memory symbol, uint256 initialSupply) 
+        public 
+        initializer 
+    {
+        __ERC20_init(name, symbol);
+        _mint(msg.sender, initialSupply);
+    }
+}
---
+++ test/Upgradeable.ts
@@ -0,0 +1,27 @@
+import { ethers, upgrades } from "hardhat";
+import { MyContractV1, MyContractV2 } from "../typechain-types";
+import { expect } from "chai";
+
+describe("MyToken Upgrade", function () {
+  let proxyAddress: string;
+
+  before(async function () {
+    const initialSupply = 1000n * 10n ** 18n;
+    const Upgradeable1 = await ethers.getContractFactory("Upgradeable1");
+    const proxy = await upgrades.deployProxy(
+      Upgradeable1,
+      ["MyToken", "MTK", initialSupply],
+      { initializer: "initialize" },
+    );
+
+    proxyAddress = await proxy.getAddress();
+  });
+
+  it("upgrades to V2", async function () {
+    const Upgradeable2 = await ethers.getContractFactory("Upgradeable2");
+    const upgraded = await upgrades.upgradeProxy(proxyAddress, Upgradeable2);
+
+    const newImplAddress = await upgraded.getAddress();
+    expect(newImplAddress).to.equal(proxyAddress);
+  });
+});
