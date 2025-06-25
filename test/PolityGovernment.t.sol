97fb70748952b6a02a0b6c9395a4f1c5d34019db
Victor Chen <victor.chen@hoc-trade.com>
2025-06-25 12:40:34 +0800

    feature: implement purpose new governors in polity

--- test/PolityGovernment.t.sol
+++ test/PolityGovernment.t.sol
@@ -44,6 +44,30 @@ contract PolityGovernmentTest is Test {
         assertEq(listed[0], initGovernor);
     }
 
+    function testListAddGovernorProposals() public {
+        address newGov1 = address(0x1);
+        address newGov2 = address(0x2);
+
+        vm.prank(initGovernor);
+        polity.proposeAddGovernor(newGov1);
+        vm.prank(initGovernor);
+        polity.proposeAddGovernor(newGov2);
+
+        (address[] memory proposeds, uint256[] memory votes, bool[] memory executed) =
+            polity.listAddGovernorProposals();
+
+        assertEq(proposeds.length, 2);
+        assertEq(votes.length, 2);
+        assertEq(executed.length, 2);
+
+        assertEq(proposeds[0], newGov1);
+        assertEq(proposeds[1], newGov2);
+        assertEq(votes[0], 0);
+        assertEq(votes[1], 0);
+        assertFalse(executed[0]);
+        assertFalse(executed[1]);
+    }
+
     function testApproveAndTriggerUpgrade() public {
         vm.prank(initGovernor);
         polity.approveUpgrade(newImpl);
