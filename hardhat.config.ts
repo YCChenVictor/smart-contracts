5a153ce90f6f3e376f0f5777c0ea6aa014daa6e6
Victor Chen <victor.chen@hoc-trade.com>
2025-07-27 15:26:27 +0800

    feature: implement-module-citizens

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,9 +2,27 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 import "@openzeppelin/hardhat-upgrades";
 import "solidity-coverage";
+import dotenv from "dotenv";
 
-const config: HardhatUserConfig = {
+dotenv.config();
+
+let config: HardhatUserConfig
+if(process.env.NODE_ENV === "test") {
+   config= {
   solidity: "0.8.24",
 };
+} else {
+ config = {
+  solidity: "0.8.24",
+  networks: {
+    hardhat: {
+      mining: {
+        auto: false,
+        interval: 1000, // or whatever ms delay you want
+      },
+    },
+  },
+};
+}
 
 export default config;
